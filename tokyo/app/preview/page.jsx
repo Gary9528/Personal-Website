b520262c33d5192d7f1b1ac58b600dd94477dab9
Gary9528 <112595926+Gary9528@users.noreply.github.com>
2025-04-01 21:24:29 -0400

    second home page debug

--- tokyo/app/preview/page.jsx
+++ tokyo/app/preview/page.jsx
@@ -1,9 +1,30 @@
-import Preview from "@/components/preview";
-import React from "react";
+import Home from "@/components/home/Home";
 
+import Sidebar from "@/components/sidebar/Sidebar";
 export const metadata = {
-  title: "Light/Dark Mode || Gary Chhina's Personal Website",
+  title: "Home || Gary Chhina's Personal Website",
 };
-export default function page() {
-  return <Preview />;
-}
+const index = () => {
+  return (
+    <>
+      {/* En Seo Page Title */}
+
+      <Sidebar />
+      {/* END LEFT MENU CONTENT */}
+
+      {/* START RIGHT PART CONTENT */}
+      <div className="rightpart">
+        <div className="rightpart_in">
+          <div className="tokyo_tm_section">
+            <div data-aos="fade-right" data-aos-duration="1200">
+              <Home />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* END RIGHT PART CONTENT */}
+    </>
+  );
+};
+
+export default index;
